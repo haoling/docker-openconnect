9f9a2d9d19fd8b055b92cbba579d5eb4bd2e8ba9
Haoling <maya@fei-yen.jp>
2018-04-12 15:47:04 +0900

    arguments from environments

--- run.sh
+++ run.sh
@@ -8,5 +8,11 @@ iptables -I FORWARD -o tun0 -j ACCEPT
 iptables -t nat -D POSTROUTING -o tun0 -j MASQUERADE
 iptables -t nat -I POSTROUTING -o tun0 -j MASQUERADE
 
+CMD="/usr/sbin/openconnect --passwd-on-stdin"
+[ -n "${SERVERCERT}" ] && CMD="${CMD} --servercert ${SERVERCERT}"
+[ -n "${USER}" ] && CMD="${CMD} --user ${USER}"
+[ -n "${AUTHGROUP}" ] && CMD="${CMD} --authgroup ${AUTHGROUP}"
+CMD="${CMD} ${ADDRESS}"
+
 echo "Starting openconect"
-echo "${PASSWORD}" | /usr/sbin/openconnect --passwd-on-stdin --no-cert-check $@
+echo "${PASSWORD}" | $CMD
